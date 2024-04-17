b70bc7871e8296cc7af93fce624b64174cbb12a0
Isabella Nightshade <47324660+xBelladonna@users.noreply.github.com>
2024-04-17 21:18:10 +1000

    Improvement to image generator regex

--- src/oobabot/image_generator.py
+++ src/oobabot/image_generator.py
@@ -259,7 +259,7 @@ class ImageGenerator:
 
         self.image_patterns = [
             re.compile(
-                r"^.*\b" + image_word + r"\b\s*((a|the|of|with)\s)*:?([\w ,\-\(\)\[\]:]+)[^\w]*$",
+                r"^.*\b" + image_word + r"\b\s*((as?|of|the|with)\s)*:?([\w\s,:\-\(\)\[\]]+)[^\w]*$",
                 re.IGNORECASE,
             )
             for image_word in self.image_words
@@ -267,7 +267,7 @@ class ImageGenerator:
         self.avatar_patterns = [
             re.compile(
                 r"\b" + avatar_word + r"[^\w]*\b",
-                re.IGNORECASE
+                re.IGNORECASE,
             )
             for avatar_word in self.avatar_words
         ]
