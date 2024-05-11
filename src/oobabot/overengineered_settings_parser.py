3b3706a0d1c39b3a16d774ae42618d3b324facad
Isabella Nightshade <47324660+xBelladonna@users.noreply.github.com>
2024-05-12 04:58:15 +1000

    Fix bug in config preventing booleans from being set away from their default

--- src/oobabot/overengineered_settings_parser.py
+++ src/oobabot/overengineered_settings_parser.py
@@ -161,7 +161,7 @@ class ConfigSetting(typing.Generic[T]):
             return
         if self.name not in yaml:
             return
-        if not self.place_default_in_yaml and not yaml[self.name]:
+        if not self.place_default_in_yaml and yaml[self.name] is None:
             # if we didn't place the default in the yaml, and the setting
             # is now blank, that's no surprise.  Keep the default
             # rather than overwriting it with None
